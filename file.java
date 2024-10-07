8c69d3051b668aaab1e7da559ae8157330babf02
notft <aibelbinzacariah@gmail.com>
2024-10-07 15:26:49 +0530

    code is working

--- file.java
+++ file.java
@@ -1,34 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-class file{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		
-		FileWriter fw = new FileWriter("test.txt");
-		System.out.println("enter lines of text (type 'stop' to end) :");
-		do {
-			s = sc.nextLine();
-			if ( !s.equals("stop")){
-				fw.write(s);
-				fw.write("\n");
-				}
-			}while (!s.equals("stop"));
-			fw.close();
-			
-			
-			try{
-				FileReader fr = new FileReader("test.txt");
-				BufferedReader br = new BufferedReader(fr);
-				System.out.println("content in the file is :-");
-				while ((s = br.readLine()) !=null){
-					System.out.println(s);
-				}
-				br.close();
-				fr.close();		
-			} catch (FileNotFoundException fe){
-				System.out.println("file not found");
-			}		
-		}
-	}
+class File {
+    public static void main(String[] args) {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+            FileWriter fw = new FileWriter("test.txt");
+            System.out.println("Enter lines of text (type 'stop' to end):");
+            
+            do {
+                s = sc.nextLine();
+                if (!s.equals("stop")) {
+                    fw.write(s);
+                    fw.write("\n");
+                }
+            } while (!s.equals("stop"));
+            
+            fw.close();
+            
+            try {
+                FileReader fr = new FileReader("test.txt");
+                BufferedReader br = new BufferedReader(fr);
+                System.out.println("Content in the file is:");
+                
+                while ((s = br.readLine()) != null) {
+                    System.out.println(s);
+                }
+                
+                br.close();
+                fr.close();
+            } catch (FileNotFoundException fe) {
+                System.out.println("File not found");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
